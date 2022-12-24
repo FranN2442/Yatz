561390f54258e1945a01ae27335ff99969e059f7
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2022-12-24 13:19:41 +0100

    Yatzy Refactored

--- src/yatzy.py
+++ src/yatzy.py
@@ -13,7 +13,11 @@ class Yatzy:
             if dice.count(dado) == 5:
                 return 50 + 5 * dado
             else:
-                return 0 
+                return 0
+    
+    @staticmethod
+    def __init__(self,*dice):
+        self.dice = list(dice)
                
 
     @staticmethod
@@ -25,15 +29,6 @@ class Yatzy:
             if res not in listaRes:
                 listaRes.append(res)
         return listaRes       
-            
-    @staticmethod
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
 
     @staticmethod
     def score_pair(*dice):
